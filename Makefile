631defb818f96fd8a6134705e25e6d8e17c052cb
Mark Geisert <mark@maxrnd.com>
2016-09-07 13:05:28 -0700

    cosmetic prep for multiple FUSE implementations

--- Makefile
+++ Makefile
@@ -1,14 +1,13 @@
-Version = $(shell sed -n '/^VERSION=/s/VERSION=\(.*\)/\1/p' fuse.cygport)
+Version = $(shell sed -n '/^VERSION=/s/VERSION=\(.*\)/\1/p' cygfuse.cygport)
 IncDir = ./inc/fuse
-#Debug = -g
+Debug = -g
 
-cygfuse-$(Version).dll libfuse-$(Version).dll.a fuse.pc: cygfuse.c fuse.pc.in
+cygfuse-$(Version).dll libfuse-$(Version).dll.a cygfuse.pc: cygfuse.c cygfuse.pc.in
 	gcc $(Debug) -shared -o cygfuse-$(Version).dll -Wl,--out-implib=libfuse-$(Version).dll.a -I$(IncDir) cygfuse.c
-	[ -n "$(Debug)" ] || strip cygfuse-$(Version).dll
-	sed "s/@Version@/$(Version)/g" fuse.pc.in > fuse.pc
+	sed "s/@Version@/$(Version)/g" cygfuse.pc.in > cygfuse.pc
 
-cygfuse-test.exe: cygfuse-test.c cygfuse-$(Version).dll libfuse-$(Version).dll.a
-	gcc $(Debug) -o cygfuse-test.exe -I$(IncDir) -DCYGFUSE cygfuse-test.c -L$(PWD) -lfuse-$(Version)
+cygfuse-test: cygfuse-test.c cygfuse-$(Version).dll libfuse-$(Version).dll.a
+	gcc $(Debug) -o cygfuse-test -I$(IncDir) -DCYGFUSE cygfuse-test.c -L$(PWD) -lfuse-$(Version)
 
 cygport:
 	git clean -dfx
@@ -18,4 +17,8 @@ cygport:
 		git archive --prefix=cygfuse-work/ --format=tar.gz $${Stash:-HEAD}\
 			> cygfuse-work.tar.gz\
 	)
-	CYGPORT_SRC_URI=cygfuse-work.tar.gz CYGPORT_SRC_DIR=cygfuse-work cygport fuse.cygport download prep compile install package
+	CYGPORT_SRC_URI=cygfuse-work.tar.gz CYGPORT_SRC_DIR=cygfuse-work cygport cygfuse.cygport download prep compile install package
+
+clean:
+	rm -f cygfuse-$(Version).dll cygfuse.pc libfuse-$(Version).dll.a
+	rm -f cygfuse-test.exe* cygfuse-work.tar.gz
--- cygfuse.c
+++ cygfuse.c
@@ -14,42 +14,77 @@
  * accordance with the commercial license agreement provided with the
  * software.
  */
+/*
+ * Modified 2016 by Mark Geisert, designated cygfuse maintainer.
+ */
 
 #include <dlfcn.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/cygwin.h>
 
+/* Add FUSE implementation initializers here. */
 static void *cygfuse_init_winfsp();
+static void *cygfuse_init_dokany();
 static void *cygfuse_init_fail();
 
 static pthread_mutex_t cygfuse_mutex = PTHREAD_MUTEX_INITIALIZER;
 static void *cygfuse_handle = 0;
+static char *fuse_variant = NULL;
+
+/* Add short names of supported FUSE implementations here. */
+#define WINFSP  "WinFSP"
+#define DOKANY  "Dokany"
 
 static inline void cygfuse_init(int force)
 {
+    fuse_variant = getenv("CYGFUSE");
+
+    if (!fuse_variant)
+    {
+        fprintf(stderr, "cygfuse: environment variable CYGFUSE is not set\n");
+        exit(1);
+    }
+
     pthread_mutex_lock(&cygfuse_mutex);
     if (force || 0 == cygfuse_handle)
-        cygfuse_handle = cygfuse_init_winfsp();
+    {
+        /* Add call to additional FUSE implementation initializers here. */
+        if (0 == strncasecmp(fuse_variant, WINFSP, strlen(WINFSP)))
+            cygfuse_handle = cygfuse_init_winfsp();
+        else if (0 == strncasecmp(fuse_variant, DOKANY, strlen(DOKANY)))
+            cygfuse_handle = cygfuse_init_dokany();
+    }
     pthread_mutex_unlock(&cygfuse_mutex);
+
+    if (0 == cygfuse_handle)
+        cygfuse_init_fail();
 }
 
 /*
  * Unfortunately Cygwin fork is very fragile and cannot even correctly
  * handle dlopen'ed DLL's if they are native (rather than Cygwin ones).
+ * [MG:] Cygwin doesn't expect non-Cygwin DLLs to be present.  That's
+ * likely a design choice but could arguably be considered a bug.  A fix
+ * would depend on the bug being reported to the main mailing list.
  *
  * So we have this very nasty hack where we reset the dlopen'ed handle
  * immediately after daemonization. This will force cygfuse_init() to
  * reload the WinFsp DLL and reset all API pointers in the daemonized
  * process.
+ * [MG:] pthread_atfork() could be used for child reinitialization after
+ * fork().  No pthreads need be involved to use this API.  This is how
+ * Cygwin apps deal with the situation.
  */
 static inline int cygfuse_daemon(int nochdir, int noclose)
 {
     if (-1 == daemon(nochdir, noclose))
         return -1;
 
-    /* force reload of WinFsp DLL to workaround fork() problems */
+    /* force reload of FUSE implementation DLL to workaround fork() problems */
     cygfuse_init(1);
 
     return 0;
@@ -59,7 +94,8 @@ static inline int cygfuse_daemon(int nochdir, int noclose)
 #define FSP_FUSE_API                    static
 #define FSP_FUSE_API_NAME(api)          (* pfn_ ## api)
 #define FSP_FUSE_API_CALL(api)          (cygfuse_init(0), pfn_ ## api)
-#define FSP_FUSE_SYM(proto, ...)        __attribute__ ((visibility("default"))) proto { __VA_ARGS__ }
+#define FSP_FUSE_SYM(proto, ...)        \
+    __attribute__ ((visibility("default"))) proto { __VA_ARGS__ }
 #include <fuse_common.h>
 #include <fuse.h>
 #include <fuse_opt.h>
@@ -69,6 +105,7 @@ static inline int cygfuse_daemon(int nochdir, int noclose)
 #else
 #define CYGFUSE_WINFSP_NAME             "winfsp-x86.dll"
 #endif
+
 #define CYGFUSE_WINFSP_PATH             "bin\\" CYGFUSE_WINFSP_NAME
 #define CYGFUSE_GET_API(h, n)           \
     if (0 == (*(void **)&(pfn_ ## n) = dlsym(h, #n)))\
@@ -84,20 +121,24 @@ static void *cygfuse_init_winfsp()
         char winpath[260], *psxpath;
         int regfd, bytes;
 
-        regfd = open("/proc/registry32/HKEY_LOCAL_MACHINE/Software/WinFsp/InstallDir", O_RDONLY);
+        regfd = open("/proc/registry32/HKEY_LOCAL_MACHINE"
+                     "/Software/WinFsp/InstallDir", O_RDONLY);
         if (-1 == regfd)
             return cygfuse_init_fail();
 
-        bytes = read(regfd, winpath, sizeof winpath - sizeof CYGFUSE_WINFSP_PATH);
+        bytes = read(regfd, winpath,
+                     sizeof winpath - sizeof CYGFUSE_WINFSP_PATH);
         close(regfd);
         if (-1 == bytes || 0 == bytes)
             return cygfuse_init_fail();
 
         if ('\0' == winpath[bytes - 1])
             bytes--;
-        memcpy(winpath + bytes, CYGFUSE_WINFSP_PATH, sizeof CYGFUSE_WINFSP_PATH);
+        memcpy(winpath + bytes,
+               CYGFUSE_WINFSP_PATH, sizeof CYGFUSE_WINFSP_PATH);
 
-        psxpath = (char *)cygwin_create_path(CCP_WIN_A_TO_POSIX | CCP_PROC_CYGDRIVE, winpath);
+        psxpath = (char *)
+            cygwin_create_path(CCP_WIN_A_TO_POSIX | CCP_PROC_CYGDRIVE, winpath);
         if (0 == psxpath)
             return cygfuse_init_fail();
 
@@ -139,8 +180,14 @@ static void *cygfuse_init_winfsp()
     return h;
 }
 
+static void *cygfuse_init_dokany()
+{
+    return 0; /* Replace this with legitimate code. */
+}
+
 static void *cygfuse_init_fail()
 {
-    abort();
-    return 0;
+    fprintf(stderr, "cygfuse: %s FUSE DLL initialization failed.\n",
+            fuse_variant);
+    exit(1);
 }
